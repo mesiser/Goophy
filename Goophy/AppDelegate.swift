dabd5c26424a2155a9bb8550ab96eb831247e38a
Vadim Shalugin <vd.shalugin@gmail.com>
2022-03-28 23:02:07 +0200

    Initial Commit

---
+++ Goophy/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Goophy
+//
+//  Created by Vadim Shalugin on 28.03.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ Goophy/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Goophy
+//
+//  Created by Vadim Shalugin on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
